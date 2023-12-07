42b3c232c6b1bbb4b97f1a01696572017a6dda56
Kittisak Kaewneam <kittisak84@gmail.com>
2023-12-07 20:25:09 +0700

    Update update_sensor.php

--- update_sensor.php
+++ update_sensor.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$mysqli = new mysqli("localhost","ddr","**123ddr321**","ddr");
+$mysqli = new mysqli("localhost","","","");
 $mysqli->set_charset('utf8');
 
 // Check connection
@@ -100,7 +100,7 @@ if($dustrec_position_id == '3'){
                             $massage1=iconv('utf-8','utf-8',$messageline);
 
                             define('LINE_API',"https://notify-api.line.me/api/notify");
-                            $token = "h7GbD4ac0qMXgx6YYqCR4Zt6niD1If0fPJzjOea1Z2t"; //ใส่ Token ของ group line ที่จะแจ้ง
+                            $token = ""; //ใส่ Token ของ group line ที่จะแจ้ง
                             $str = $massage1; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
                             $res = notify_message($str,$token);
                             //print_r($res);
