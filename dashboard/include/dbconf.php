ffb11eb4ff89c64b1ae3548f292a07232d0cc3d2
Kittisak Kaewneam <kittisak84@gmail.com>
2023-12-07 20:19:33 +0700

    Update dbconf.php

--- dashboard/include/dbconf.php
+++ dashboard/include/dbconf.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'ddr');
-define('DB_PASSWORD', '**123ddr321**');
-define('DB_DATABASE', 'ddr');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
 
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die (mysql_error());
 $connection->set_charset('utf8');
